cf91fb8a67218446f705a3a0b78b1e13a9a1f2c1
nowf41 <111325821+nowf41@users.noreply.github.com>
2025-01-26 22:26:15 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# THIS FILE IS PUBLIC DOMAIN.
+# OTHER FILES EXCEPT THIS FILE ARE **NOT** PUBLIC DOMAIN.
+
+# install packages
+sudo apt update
+sudo apt install build-essential ruby -y
+sudo gem install shell
+sudo bash ev3rt-prepare-ubuntu.sh
+
+
+# PATH
+echo "export PATH=\"\$PATH:/opt/gcc-arm-none-eabi-6-2017-q1-update/bin\"" >> ~/.bashrc
+source ~/.bashrc
