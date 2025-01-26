609e0a65b8a2bc2065aaea8a1578225aaba85788
Wataru Tsubokawa <111325821+nowf41@users.noreply.github.com>
2025-01-26 22:32:40 +0900

    progress commit

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt install build-essential ruby -y
 sudo gem install shell
 sudo bash ev3rt-prepare-ubuntu.sh
 
+tar xvf hrp3.tar.xz
 
 # PATH
 echo "export PATH=\"\$PATH:/opt/gcc-arm-none-eabi-6-2017-q1-update/bin\"" >> ~/.bashrc
